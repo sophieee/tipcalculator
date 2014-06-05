c220e41f075dab22ccab0b671de8c7f122172cfb
Sophie Tang <sophietang@measurefrom-lm.corp.yahoo.com>
2014-06-04 23:40:04 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sophie Tang on 6/4/14.
+//  Copyright (c) 2014 Sophie Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
